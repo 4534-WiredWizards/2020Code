80c2b7aa78602652f710d8d1b28500df4b78abb3
NiaLoy <73674160+NiaLoy@users.noreply.github.com>
2021-02-07 13:47:28 -0500

    auto routines!
    added a (very) simple auto test to try out next time I have time on lobot!

--- 2020Code/src/main/java/frc/robot/commands/AutoTest.java
+++ 2020Code/src/main/java/frc/robot/commands/AutoTest.java
@@ -12,15 +12,63 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class AutoTest extends SequentialCommandGroup {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
 
-  public static final double DriveDist = 100;
-  public static final double TurnAngle = -90;
+  public static final double DriveDist = 150; //scale 106%
+  public static final double TurnAngle = 90;
   
 
   public AutoTest() {
     addCommands(
-      new DriveDistance(DriveDist),
-      new TurnAngle(TurnAngle),
-      new DriveArc(40, 60)
+      new DriveDistance(150), //move forward first step
+
+      new TurnAngle(90), //turn right 90 degrees 
+
+      new DriveDistance(50), //move forward 
+
+      new TurnAngle(90), //turn right 90 degrees
+
+      new DriveDistance(70), //drive forward
+
+      new TurnAngle(90), //turn right 90 degrees
+
+      new DriveDistance(70), //drive forward
+
+      new TurnAngle(90), //turn right 90 degrees
+
+      new DriveDistance(175), //move forward 
+
+      new TurnAngle(-90), //turn left 90 degrees
+
+      new DriveDistance(50), //drive forward
+
+      new TurnAngle(-90), //turn left 90 degrees 
+
+      new DriveDistance(50), //drive forward 
+
+      new TurnAngle(-90), //turn left 90 degrees
+
+      new DriveDistance(50), //move forward
+
+      new TurnAngle(-90), //turn left 90 
+
+      new DriveDistance(100), //move forward
+
+      new TurnAngle(90), //turn right 90 
+
+      new DriveDistance(50), //move forward
+
+      new TurnAngle(-90), //turn left 90
+
+      new DriveDistance(50), //move forward
+
+      new TurnAngle(-90), //turn left 
+
+      new DriveDistance(50), //move forward
+      
+      new TurnAngle(-90), //turn left
+
+      new DriveDistance(250), //back to the start!
+
+      new DriveArc(40, 60) //this is a lie 
     );
   }
 }
--- 2020Code/src/main/java/frc/robot/commands/Barrel2021.java
+++ 2020Code/src/main/java/frc/robot/commands/Barrel2021.java
@@ -3,5 +3,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class Barrel2021 extends SequentialCommandGroup {
-    
+    public static final double DriveDist = 150;
+    public static final double TurnAngle = -90;
+
+    public Barrel2021() {
+        new DriveDistance(150);
+        new TurnAngle(90);
+        new DriveArc(40, 60);
+        
+    }
 }
