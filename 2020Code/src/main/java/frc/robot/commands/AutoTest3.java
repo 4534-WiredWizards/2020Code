1b217077f75ad513c75f0701cf62ac5af88934d5
SahilP113 <65868166+SahilP113@users.noreply.github.com>
2021-04-06 11:00:05 -0400

    Revert "Changes to Slalom and Bounce Paths"
    This reverts commit cbd09b0f76b8efad9f84b944f7c5c7f85972b834.

--- 2020Code/src/main/java/frc/robot/commands/AutoTest3.java
+++ 2020Code/src/main/java/frc/robot/commands/AutoTest3.java
@@ -18,14 +18,14 @@ public class AutoTest3 extends SequentialCommandGroup {
   
     public AutoTest3() {
         addCommands(
-           new DriveArc(45, -90), //90 degree circle counterclowise
-           new TurnAngle(171), //170 degrees clockwise
+           new DriveArc(35, -90), //90 degree circle counterclowise
+           new TurnAngle(170), //170 degrees clockwise
            new DriveDistance(65), //drive forward 65 inches
            new DriveArc(40, -180), //180 degrees counterclockwise
            new DriveDistance(65), //drive forward 65 inches
            new TurnAngle(180), //Turn 180 degrees
            new DriveDistance(65), //drive forward 65 inches
-           new DriveArc(45, -180), //180 degrees counterclockwise
+           new DriveArc(35, -180), //180 degrees counterclockwise
            new DriveDistance(65), //drive forward 65 inches
            new TurnAngle(180), //Turn 180 degrees
            new DriveArc(35, -90) //90 degrees counterclockwise
